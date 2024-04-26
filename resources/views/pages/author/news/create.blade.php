75fad501e36c1eb2984afb65dff2c6ca64976c3c
paksiswanto <unitedkuryu@gmail.com>
2024-04-26 15:46:05 +0700

    Update create.blade.php

--- resources/views/pages/author/news/create.blade.php
+++ resources/views/pages/author/news/create.blade.php
@@ -79,7 +79,6 @@
                                     class="select2 form-control category @error('category') is-invalid @enderror"
                                     name="category[]" multiple="true" value="{{ old('category') }}"
                                     aria-label="Default select example">
-                                    <option selected>pilih kategori</option>
                                     @foreach ($categories as $category)
                                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                                     @endforeach
@@ -98,7 +97,6 @@
                                     class="form-control sub-category select2 @error('sub_category') is-invalid @enderror"
                                     name="sub_category[]" multiple="true" value="{{ old('sub_category') }}"
                                     aria-label="Default select example">
-                                    <option selected>pilih sub kategori</option>
                                 </select>
                                 @error('sub_category_id')
                                     <span class="invalid-feedback" role="alert">
@@ -247,6 +245,7 @@
 @section('script')
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
 
     <script>
