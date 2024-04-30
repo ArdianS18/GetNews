1fb2cbb8938f75f5f21f6475f7de90279b6f8b3b
paksiswanto <unitedkuryu@gmail.com>
2024-04-30 15:43:32 +0700

    fix bug

--- resources/views/pages/user/news/category.blade.php
+++ resources/views/pages/user/news/category.blade.php
@@ -94,7 +94,7 @@
                                                             d="M368.005 272h-96v96h96v-96zm-32-208v32h-160V64h-48v32h-24.01c-22.002 0-40 17.998-40 40v272c0 22.002 17.998 40 40 40h304.01c22.002 0 40-17.998 40-40V136c0-22.002-17.998-40-40-40h-24V64h-48zm72 344h-304.01V196h304.01v212z"
                                                             fill="#E93314" />
                                                     </svg></i><a
-                                                    href="javascript:void(0)">{{ $news->created_at_formatted }}</a>
+                                                    href="javascript:void(0)">{{  \Carbon\Carbon::parse($news->upload_date)->translatedFormat('d F Y') }}</a>
                                             </li>
                                             <li>
                                                 <i class="fi fi-rr-eye">
