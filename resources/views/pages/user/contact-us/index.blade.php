02a3afa323fd8b4c129dca5e7e3a584b933d7693
jovitamaharani <jovitamaharani321@gmail.com>
2024-04-30 15:50:02 +0700

    contact us

--- resources/views/pages/user/contact-us/index.blade.php
+++ resources/views/pages/user/contact-us/index.blade.php
@@ -24,38 +24,38 @@
             </div>
 
             <div class="col-md-12 col-lg-6">
-                <div class="card p-5">
+                <div class="contact-card">
                     <h3> <span style="color: #0F4D8A;">|</span>Contact</h3>
 
                     <div class="row mt-5">
                         <div class="col-md-12 col-lg-4 d-flex">
-                            <div class="d-flex justify-content-center align-items-center" style="background-color: #0F4D8A; width:50px; height: 50px; border-radius:5px;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#ffffff" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3"/></svg>
+                            <div class="d-flex justify-content-center align-items-center" style="background-color: #0F4D8A; width:65px; height: 50px; border-radius:5px;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="#ffffff" d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3"/></svg>
                             </div>
 
                             <div class="" style="margin-left: 7px;">
                                 <b style="font-size: 14px;">Phone</b>
-                                <p style="font-size: 14px;">+62-123456789</p>
+                                <span style="font-size: 14px;">+62123456789</span>
                             </div>
                         </div>
 
                         <div class="col-md-12 col-lg-4 d-flex">
                             <div class="d-flex justify-content-center align-items-center" style="background-color: #0F4D8A; width:50px; height: 50px; border-radius:5px;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 12q.825 0 1.413-.587T14 10q0-.825-.587-1.412T12 8q-.825 0-1.412.588T10 10q0 .825.588 1.413T12 12m0 7.35q3.05-2.8 4.525-5.087T18 10.2q0-2.725-1.737-4.462T12 4Q9.475 4 7.738 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35M12 22q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2q3.175 0 5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22m0-12"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="#ffffff" d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.587 1.413T20 20zm8-7L4 8v10h16V8zm0-2l8-5H4zM4 8V6v12z"/></svg>
                             </div>
 
                             <div class="d-flex flex-column " style="margin-left: 7px;">
-                                <b>Email</b>
+                                <b style="font-size: 14px;">Email</b>
                                 <p style="font-size: 14px;">get@gmail.com</p>
                             </div>
                         </div>
 
                         <div class="col-md-12 col-lg-4 d-flex">
                             <div class="d-flex justify-content-center align-items-center" style="background-color: #0F4D8A; width:50px; height: 50px; border-radius:5px;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 12q.825 0 1.413-.587T14 10q0-.825-.587-1.412T12 8q-.825 0-1.412.588T10 10q0 .825.588 1.413T12 12m0 7.35q3.05-2.8 4.525-5.087T18 10.2q0-2.725-1.737-4.462T12 4Q9.475 4 7.738 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35M12 22q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2q3.175 0 5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22m0-12"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 12q.825 0 1.413-.587T14 10q0-.825-.587-1.412T12 8q-.825 0-1.412.588T10 10q0 .825.588 1.413T12 12m0 7.35q3.05-2.8 4.525-5.087T18 10.2q0-2.725-1.737-4.462T12 4Q9.475 4 7.738 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35M12 22q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2q3.175 0 5.588 2.225T20 10.2q0 2.5-1.987 5.438T12 22m0-12"/></svg>
                             </div>
                             <div class="d-flex flex-column " style="margin-left: 7px;">
-                                <b>Location</b>
+                                <b style="font-size: 14px;">Location</b>
                                 <p style="font-size: 14px;">jager kulon omah</p>
                             </div>
                         </div>
@@ -73,7 +73,7 @@
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="form-group">
-                                <textarea name="message" id="message" cols="30" rows="6" placeholder="" style="resize: none"></textarea>
+                                <textarea name="message" id="message" class="form-control" cols="100" rows="6" placeholder="" style="resize: none"></textarea>
                                 </div>
                             </div>
                             <div class="col-md-12 mt-3">
--- resources/views/pages/user/news/category.blade.php
+++ resources/views/pages/user/news/category.blade.php
@@ -36,7 +36,7 @@
                     @empty
                         <div class="d-flex justify-content-center">
                             <div>
-                                <img src="{{ asset('no-data.svg') }}" width="550px" alt="">
+                                <img src="{{ asset('assets/img/no-data.svg') }}" alt="">
                             </div>
                         </div>
                         <div class="text-center">
@@ -83,7 +83,7 @@
                                         <img src="{{ asset('storage/' . $news->photo) }}"width="100%" height="80">
                                     </div>
                                     <div class="news-card-info">
-                                        <h3><a href="#">{{ $news->name }}</a>
+                                        <h3><a href="#">{!! Illuminate\Support\Str::limit($news->name, $limit = 20, $end = '...')  !!}</a>
                                         </h3>
                                         <ul class="news-metainfo list-style">
                                             <li>
