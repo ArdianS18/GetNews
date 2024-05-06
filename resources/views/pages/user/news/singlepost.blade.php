81ed43ca488c2a6be63e6eac27c1d1ffa7ec8f09
paksiswanto <unitedkuryu@gmail.com>
2024-05-06 15:26:27 +0700

    Update singlepost.blade.php

--- resources/views/pages/user/news/singlepost.blade.php
+++ resources/views/pages/user/news/singlepost.blade.php
@@ -90,6 +90,80 @@
         .breadcrumb-menu {
             text-align: start !important
         }
+
+        .shareLink {
+            .permalink {
+                position: relative;
+                border-radius: 30px;
+
+                .textLink {
+                    text-align: center;
+                    padding: 12px 60px 12px 30px;
+                    height: 45px;
+                    width: 450px;
+                    font-size: 16px;
+                    letter-spacing: .3px;
+                    color: #494949;
+                    border-radius: 25px;
+                    border: 1px solid #f2f2f2;
+                    background-color: #f2f2f2;
+                    outline: 0;
+                    appearance: none;
+                    transition: all .3s ease;
+
+                    @media (max-width: 767px) {
+                        width: 100%;
+                    }
+
+                    &:focus {
+                        border-color: #d8d8d8;
+                    }
+
+                    &::selection {
+                        color: #fff;
+                        background-color: #ff0a4b;
+                    }
+                }
+
+                .copyLink {
+                    position: absolute;
+                    top: 50%;
+                    right: 25px;
+                    cursor: pointer;
+                    transform: translateY(-50%);
+
+                    &:hover {
+                        &:after {
+                            opacity: 1;
+                            transform: translateY(0) translateX(-50%);
+                        }
+                    }
+
+                    &:after {
+                        content: attr(tooltip);
+                        width: 140px;
+                        bottom: -40px;
+                        left: 50%;
+                        padding: 5px;
+                        border-radius: 4px;
+                        font-size: 0.8rem;
+                        opacity: 0;
+                        pointer-events: none;
+                        position: absolute;
+                        background-color: #000000;
+                        color: #ffffff;
+                        transform: translateY(-10px) translateX(-50%);
+                        transition: all 300ms ease;
+                        text-align: center;
+                    }
+
+                    i {
+                        font-size: 20px;
+                        color: #ff0a4b;
+                    }
+                }
+            }
+        }
     </style>
 @endsection
 @section('content')
@@ -313,8 +387,22 @@
 
                                         <div class="container">
                                             <div class="mb-3 form-group">
-                                                <label for="message" class="form-label">Url</label>
-                                                <input type="text" name="share" id="input-link">
+                                                <label for="message" class="form-label fw-bold">Url</label>
+                                                <div class="shareLink">
+                                                    <div class="permalink">
+                                                        <input class="textLink" type="text" name="shortlink"
+                                                            value="https://media.mijurnal.com/berita/{{ $news->slug }}"
+                                                            id="copy-link" readonly="">
+                                                        <span class="copyLink" onclick="copyToClipboard()" id="copy"
+                                                            tooltip="Copy to clipboard">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="20"
+                                                                height="20" viewBox="0 0 32 32">
+                                                                <path fill="#000000"
+                                                                    d="M28 10v18H10V10zm0-2H10a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2" />
+                                                                <path fill="#000000" d="M4 18H2V4a2 2 0 0 1 2-2h14v2H4Z" />
+                                                            </svg> </span>
+                                                    </div>
+                                                </div>
                                                 @error('message')
                                                     <span class="invalid-feedback" role="alert" style="color: red;">
                                                         <strong>{{ $message }}</strong>
@@ -361,43 +449,6 @@
                                                             </svg>Facebook</button>
                                                     </div>
                                                     <div class="col-md-6 col-12 mb-3">
-                                                        <button class="btn shadow-sm gap-3" id="ig"
-                                                            style="width:-webkit-fill-available !important"><svg
-                                                                xmlns="http://www.w3.org/2000/svg" width="32"
-                                                                height="32" viewBox="0 0 256 256">
-                                                                <g fill="none">
-                                                                    <rect width="256" height="256"
-                                                                        fill="url(#skillIconsInstagram0)"
-                                                                        rx="60" />
-                                                                    <rect width="256" height="256"
-                                                                        fill="url(#skillIconsInstagram1)"
-                                                                        rx="60" />
-                                                                    <path fill="#fff"
-                                                                        d="M128.009 28c-27.158 0-30.567.119-41.233.604c-10.646.488-17.913 2.173-24.271 4.646c-6.578 2.554-12.157 5.971-17.715 11.531c-5.563 5.559-8.98 11.138-11.542 17.713c-2.48 6.36-4.167 13.63-4.646 24.271c-.477 10.667-.602 14.077-.602 41.236s.12 30.557.604 41.223c.49 10.646 2.175 17.913 4.646 24.271c2.556 6.578 5.973 12.157 11.533 17.715c5.557 5.563 11.136 8.988 17.709 11.542c6.363 2.473 13.631 4.158 24.275 4.646c10.667.485 14.073.604 41.23.604c27.161 0 30.559-.119 41.225-.604c10.646-.488 17.921-2.173 24.284-4.646c6.575-2.554 12.146-5.979 17.702-11.542c5.563-5.558 8.979-11.137 11.542-17.712c2.458-6.361 4.146-13.63 4.646-24.272c.479-10.666.604-14.066.604-41.225s-.125-30.567-.604-41.234c-.5-10.646-2.188-17.912-4.646-24.27c-2.563-6.578-5.979-12.157-11.542-17.716c-5.562-5.562-11.125-8.979-17.708-11.53c-6.375-2.474-13.646-4.16-24.292-4.647c-10.667-.485-14.063-.604-41.23-.604zm-8.971 18.021c2.663-.004 5.634 0 8.971 0c26.701 0 29.865.096 40.409.575c9.75.446 15.042 2.075 18.567 3.444c4.667 1.812 7.994 3.979 11.492 7.48c3.5 3.5 5.666 6.833 7.483 11.5c1.369 3.52 3 8.812 3.444 18.562c.479 10.542.583 13.708.583 40.396c0 26.688-.104 29.855-.583 40.396c-.446 9.75-2.075 15.042-3.444 18.563c-1.812 4.667-3.983 7.99-7.483 11.488c-3.5 3.5-6.823 5.666-11.492 7.479c-3.521 1.375-8.817 3-18.567 3.446c-10.542.479-13.708.583-40.409.583c-26.702 0-29.867-.104-40.408-.583c-9.75-.45-15.042-2.079-18.57-3.448c-4.666-1.813-8-3.979-11.5-7.479s-5.666-6.825-7.483-11.494c-1.369-3.521-3-8.813-3.444-18.563c-.479-10.542-.575-13.708-.575-40.413c0-26.704.096-29.854.575-40.396c.446-9.75 2.075-15.042 3.444-18.567c1.813-4.667 3.983-8 7.484-11.5c3.5-3.5 6.833-5.667 11.5-7.483c3.525-1.375 8.819-3 18.569-3.448c9.225-.417 12.8-.542 31.437-.563zm62.351 16.604c-6.625 0-12 5.37-12 11.996c0 6.625 5.375 12 12 12s12-5.375 12-12s-5.375-12-12-12zm-53.38 14.021c-28.36 0-51.354 22.994-51.354 51.355c0 28.361 22.994 51.344 51.354 51.344c28.361 0 51.347-22.983 51.347-51.344c0-28.36-22.988-51.355-51.349-51.355zm0 18.021c18.409 0 33.334 14.923 33.334 33.334c0 18.409-14.925 33.334-33.334 33.334c-18.41 0-33.333-14.925-33.333-33.334c0-18.411 14.923-33.334 33.333-33.334" />
-                                                                    <defs>
-                                                                        <radialGradient id="skillIconsInstagram0"
-                                                                            cx="0" cy="0" r="1"
-                                                                            gradientTransform="matrix(0 -253.715 235.975 0 68 275.717)"
-                                                                            gradientUnits="userSpaceOnUse">
-                                                                            <stop stop-color="#FD5" />
-                                                                            <stop offset=".1" stop-color="#FD5" />
-                                                                            <stop offset=".5" stop-color="#FF543E" />
-                                                                            <stop offset="1" stop-color="#C837AB" />
-                                                                        </radialGradient>
-                                                                        <radialGradient id="skillIconsInstagram1"
-                                                                            cx="0" cy="0" r="1"
-                                                                            gradientTransform="matrix(22.25952 111.2061 -458.39518 91.75449 -42.881 18.441)"
-                                                                            gradientUnits="userSpaceOnUse">
-                                                                            <stop stop-color="#3771C8" />
-                                                                            <stop offset=".128" stop-color="#3771C8" />
-                                                                            <stop offset="1" stop-color="#60F"
-                                                                                stop-opacity="0" />
-                                                                        </radialGradient>
-                                                                    </defs>
-                                                                </g>
-                                                            </svg>Instagram</button>
-                                                    </div>
-                                                    <div class="col-md-6 col-12 mb-3">
                                                         <button class="btn shadow-sm gap-3" id="tele"
                                                             style="width:-webkit-fill-available !important"><svg
                                                                 xmlns="http://www.w3.org/2000/svg" width="32"
@@ -414,27 +465,6 @@
                                                                 <path fill="#FFF"
                                                                     d="M57.94 126.648c37.32-16.256 62.2-26.974 74.64-32.152c35.56-14.786 42.94-17.354 47.76-17.441c1.06-.017 3.42.245 4.96 1.49c1.28 1.05 1.64 2.47 1.82 3.467c.16.996.38 3.266.2 5.038c-1.92 20.24-10.26 69.356-14.5 92.026c-1.78 9.592-5.32 12.808-8.74 13.122c-7.44.684-13.08-4.912-20.28-9.63c-11.26-7.386-17.62-11.982-28.56-19.188c-12.64-8.328-4.44-12.906 2.76-20.386c1.88-1.958 34.64-31.748 35.26-34.45c.08-.338.16-1.598-.6-2.262c-.74-.666-1.84-.438-2.64-.258c-1.14.256-19.12 12.152-54 35.686c-5.1 3.508-9.72 5.218-13.88 5.128c-4.56-.098-13.36-2.584-19.9-4.708c-8-2.606-14.38-3.984-13.82-8.41c.28-2.304 3.46-4.662 9.52-7.072" />
                                                             </svg>Telegram</button>
-                                                    </div>
-                                                    <div class="col-md-6 col-12 mb-3">
-                                                        <button class="btn shadow-sm gap-3" id="dc"
-                                                            style="width:-webkit-fill-available !important"><svg
-                                                                xmlns="http://www.w3.org/2000/svg" width="32"
-                                                                height="32" viewBox="0 0 256 256">
-                                                                <g fill="none">
-                                                                    <rect width="256" height="256" fill="#5865F2"
-                                                                        rx="60" />
-                                                                    <g clip-path="url(#skillIconsDiscord0)">
-                                                                        <path fill="#fff"
-                                                                            d="M197.308 64.797a164.918 164.918 0 0 0-40.709-12.627a.618.618 0 0 0-.654.31c-1.758 3.126-3.706 7.206-5.069 10.412c-15.373-2.302-30.666-2.302-45.723 0c-1.364-3.278-3.382-7.286-5.148-10.412a.643.643 0 0 0-.655-.31a164.472 164.472 0 0 0-40.709 12.627a.583.583 0 0 0-.268.23c-25.928 38.736-33.03 76.52-29.546 113.836a.685.685 0 0 0 .26.468c17.106 12.563 33.677 20.19 49.94 25.245a.648.648 0 0 0 .702-.23c3.847-5.254 7.276-10.793 10.217-16.618a.633.633 0 0 0-.347-.881c-5.44-2.064-10.619-4.579-15.601-7.436a.642.642 0 0 1-.063-1.064a86.364 86.364 0 0 0 3.098-2.428a.618.618 0 0 1 .646-.088c32.732 14.944 68.167 14.944 100.512 0a.617.617 0 0 1 .655.08a79.613 79.613 0 0 0 3.106 2.436a.642.642 0 0 1-.055 1.064a102.622 102.622 0 0 1-15.609 7.428a.638.638 0 0 0-.339.889a133.075 133.075 0 0 0 10.208 16.61a.636.636 0 0 0 .702.238c16.342-5.055 32.913-12.682 50.02-25.245a.646.646 0 0 0 .26-.46c4.17-43.141-6.985-80.616-29.571-113.836a.506.506 0 0 0-.26-.238M94.834 156.142c-9.855 0-17.975-9.047-17.975-20.158s7.963-20.158 17.975-20.158c10.09 0 18.131 9.127 17.973 20.158c0 11.111-7.962 20.158-17.973 20.158m66.456 0c-9.855 0-17.974-9.047-17.974-20.158s7.962-20.158 17.974-20.158c10.09 0 18.131 9.127 17.974 20.158c0 11.111-7.884 20.158-17.974 20.158" />
-                                                                    </g>
-                                                                    <defs>
-                                                                        <clipPath id="skillIconsDiscord0">
-                                                                            <path fill="#fff"
-                                                                                d="M28 51h200v154.93H28z" />
-                                                                        </clipPath>
-                                                                    </defs>
-                                                                </g>
-                                                            </svg>Discord</button>
                                                     </div>
                                                     <div class="col-md-6 col-12 mb-3">
                                                         <button id="tw" class="btn shadow-sm gap-3"
@@ -816,6 +846,26 @@
 
         });
 
+
+        function copyToClipboard() {
+            var copyText = document.querySelector("#copy-link");
+            console.log(copyText);
+            copyText.select();
+            copyText.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+            Swal.fire({
+                icon: 'success',
+                title: 'Link disalin!',
+                text: 'Link telah disalin ke clipboard',
+                onClose: () => {
+                    var modal = document.getElementById('share');
+                    modal.style.display = 'none'; // Menyembunyikan modal
+                }
+
+            });
+
+        }
+
         document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('wa').addEventListener('click', function() {
                 shareOnWhatsapp('your_news_id_here');
@@ -825,17 +875,14 @@
                 shareOnFacebook('your_news_id_here');
             });
 
-            document.getElementById('ig').addEventListener('click', function() {
-                shareOnInstagram('your_news_id_here');
-            });
+
 
             document.getElementById('tele').addEventListener('click', function() {
                 shareOnTelegram('your_news_id_here');
             });
 
-            document.getElementById('dc').addEventListener('click', function() {
-                shareOnDiscord('your_news_id_here');
-            });
+
+
 
             document.getElementById('tw').addEventListener('click', function() {
                 shareOnTwitter('your_news_id_here');
@@ -867,15 +914,6 @@
             window.open(twitterLink, '_blank');
         }
 
-        function shareOnDiscord(newsId) {
-            var url = 'https://media.mijurnal.com/berita/{{ $news->slug }}';
-            var text = "Baca Selengkapnya di: " + url
-
-            var discordLink =
-                'https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot&permissions=2048&redirect_uri=' +
-                encodeURIComponent(text);
-            window.open(discordLink, '_blank');
-        }
 
         function shareOnTelegram(newsId) {
             var url = 'https://media.mijurnal.com/berita/{{ $news->slug }}';
@@ -883,14 +921,6 @@
 
             var telegramLink = 'https://t.me/share/url?url=' + encodeURIComponent(text);
             window.open(telegramLink, '_blank');
-        }
-
-        function shareOnInstagram(newsId) {
-            var url = 'https://media.mijurnal.com/berita/{{ $news->slug }}';
-            var text = "Baca Selengkapnya di: " + url
-
-            var instagramLink = 'https://www.instagram.com/?url=' + encodeURIComponent(text);
-            window.open(instagramLink, '_blank');
         }
     </script>
 
