98367f3e548d7821a530080c90258430688db9ee
ArdianS18 <ardiansupriadi464@gmail.com>
2024-05-06 08:41:54 +0700

    Update detail-news.blade.php

--- resources/views/pages/admin/news_admin/detail-news.blade.php
+++ resources/views/pages/admin/news_admin/detail-news.blade.php
@@ -52,7 +52,7 @@
                     @else
                         <div>
                             <div class="d-flex gap-2 mb-3">
-                            <a href="/list-news-approved" class="btn btn-lg px-3 text-white"
+                            <a href="/news-approved-list" class="btn btn-lg px-3 text-white"
                                 style="background-color: #5D87FF;">Kembali</a>
                             </div>
                         </div>
