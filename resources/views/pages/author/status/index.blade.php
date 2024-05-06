20750207ac340b9b2c592b7b6cea2c05ccaa2e23
paksiswanto <unitedkuryu@gmail.com>
2024-05-06 10:23:05 +0700

    Update index.blade.php

--- resources/views/pages/author/status/index.blade.php
+++ resources/views/pages/author/status/index.blade.php
@@ -166,27 +166,3 @@
         @endforelse
     </div>
 @endsection
-
-@section('script')
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-
-    <script>
-        $(document).ready(function() {
-            $('#content').summernote({
-                height: 400,
-                toolbar: [
-                    ['style', ['style']],
-                    ['font', ['bold', 'underline', 'clear']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['table', ['table']],
-                    ['insert', ['link', 'picture', 'video']],
-                    ['view', ['fullscreen', 'codeview', 'help']]
-                ]
-
-            });
-        });
-    </script>
-@endsection
