5e034f3b354eb2b594969bcab2a6aba133d0ce99
paksiswanto <unitedkuryu@gmail.com>
2024-05-12 18:08:24 +0700

    Update ViewInterface.php

--- app/Contracts/Interfaces/ViewInterface.php
+++ app/Contracts/Interfaces/ViewInterface.php
@@ -14,6 +14,5 @@ interface ViewInterface extends GetInterface, StoreInterface, UpdateInterface, S
     public function showCountView() : mixed;
     public function trending() : mixed;
     public function where() : mixed;
-    public function showNewsStatistic($user,Request $request) : mixed;
 
 }
