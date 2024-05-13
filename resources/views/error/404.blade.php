dd6095611386d4478170fb4c57a595a6e19c3ab8
ArdianS18 <ardiansupriadi464@gmail.com>
2024-05-13 15:35:20 +0700

    Update 404.blade.php

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -16,34 +16,37 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-8 offset-lg-2 mt-4">
+            <div class="col-lg-10 offset-lg-1 mt-4">
                 <h5 align="center" class="mb-4">Berita Direkomendasikan</h5>
                 <hr>
                 <div class="row">
                     @foreach ($news as $news)
                     @php
                     $dateParts = date_parse($news->upload_date);
-                @endphp
-                   <div class="col-md-6 col-12">
-                    <div class="news-card-one">
-                        <div class="news-card-img">
-                            <img src="{{ asset('storage/'.$news->photo) }}" width="100%" height="100px" style="object-fit: cover;">
-                        </div>
-                        <div class="news-card-info">
-                            <h3><a data-toggle="tooltip" data-placement="top" title="{{ $news->name }}" href="{{ route('news.user', ['news' => $news->slug,'year'=> $dateParts['year'],'month'=>$dateParts['month'],'day'=> $dateParts['day'] ]) }}">{!! Illuminate\Support\Str::limit($news->name, $limit = 40, $end = '...')  !!}</a>
-                            </h3>
-                            <ul class="news-metainfo list-style">
-                                <li><i class="fi fi-rr-calendar-minus"></i>
-                                    <a href="javascript:void(0)">{{  \Carbon\Carbon::parse($news->upload_date)->translatedFormat('d F Y') }}</a>
-                                </li>
-                                <li>
-                                    <i class="fi fi-rr-eye">
-                                    </i><a href="news-by-dateus">{{ $news->views->count() }}</a>
-                                </li>
-                            </ul>
+                    @endphp
+
+                    <div class="col-md-6 col-12">
+                        <div class="news-card-five">
+                            <div class="news-card-img">
+                                <img src="{{ asset('storage/' . $news->photo) }}" alt="Image" width="100%" height="150" style="object-fit: cover"/>
+                                <a data-toggle="tooltip" data-placement="top" title="{{ $news->newsCategories[0]->category->name }}" href="{{ route('categories.show.user', ['category' => $news->newsCategories[0]->category->slug,]) }}" class="news-cat">{{ $news->newsCategories[0]->category->name }}</a>
+                            </div>
+                            <div class="news-card-info">
+                                <h3><a data-toggle="tooltip" data-placement="top" title="{{ $news->name }}" href="{{ route('news.user', ['news' => $news->slug,'year'=> $dateParts['year'],'month'=>$dateParts['month'],'day'=> $dateParts['day'] ]) }}">
+                                    {!! Illuminate\Support\Str::limit($news->name, $limit = 60, $end = '...')  !!}
+                                </a></h3>
+                                <p>
+                                    {!! Illuminate\Support\Str::limit(strip_tags($news->content), 50, '...') !!}
+                                </p>
+                                <ul class="news-metainfo list-style">
+                                    <li><i class="fi fi-rr-calendar-minus"></i><a
+                                        href="javascript:void(0)">{{ \Carbon\Carbon::parse($news->upload_date)->translatedFormat('d F Y') }}</a>
+                                    </li>
+                                    <li><i class="fi fi-rr-eye"></i>{{ $news->views->count() }}</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                   </div>
                     @endforeach
                 </div>
             </div>
