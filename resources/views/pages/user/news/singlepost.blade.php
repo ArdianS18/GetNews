68b273d719ab1f40d8e729247a58b0c9d9e9e588
paksiswanto <unitedkuryu@gmail.com>
2024-05-20 08:20:13 +0700

    Update singlepost.blade.php

--- resources/views/pages/user/news/singlepost.blade.php
+++ resources/views/pages/user/news/singlepost.blade.php
@@ -971,7 +971,7 @@
 
             var url =
                 `https://media.mijurnal.com/{{ $dateParts['year'] }}/{{ $dateParts['month'] }}/{{ $dateParts['day'] }}/${dataSlug}`;
-            var text = `${dataName}, Baca Selengkapnya di:  + ${url}`
+            var text = `${dataName}, Baca Selengkapnya di: ${url}`
 
             var whatsappLink = 'https://api.whatsapp.com/send?text=' + encodeURIComponent(text);
 
@@ -996,7 +996,7 @@
 
             var url =
                 `https://media.mijurnal.com/{{ $dateParts['year'] }}/{{ $dateParts['month'] }}/{{ $dateParts['day'] }}/${dataSlug}`;
-            var text = `${dataName}, Baca Selengkapnya di:  + ${url}`
+            var text = `${dataName}, Baca Selengkapnya di:  ${url}`
 
             var twitterLink = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(text);
             window.open(twitterLink, '_blank');
@@ -1010,7 +1010,7 @@
 
             var url =
                 `https://media.mijurnal.com/{{ $dateParts['year'] }}/{{ $dateParts['month'] }}/{{ $dateParts['day'] }}/${dataSlug}`;
-            var text = `${dataName}, Baca Selengkapnya di:  + ${url}`
+            var text = `${dataName}, Baca Selengkapnya di: ${url}`
 
             var telegramLink = 'https://t.me/share/url?url=' + encodeURIComponent(text);
             window.open(telegramLink, '_blank');
