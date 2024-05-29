168d4ea4a541a426dcb7446cc65050b6f637d1b9
paksiswanto <unitedkuryu@gmail.com>
2024-05-29 11:43:29 +0700

    Update AuthorRequest.php

--- app/Http/Requests/AuthorRequest.php
+++ app/Http/Requests/AuthorRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AuthorRequest extends ApiRequest
+class AuthorRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
