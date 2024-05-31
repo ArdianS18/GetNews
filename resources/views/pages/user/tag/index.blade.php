fc18b7a55229843cb2b194c5f62636109f126e6d
paksiswanto <unitedkuryu@gmail.com>
2024-05-31 08:39:14 +0700

    Update index.blade.php

--- resources/views/pages/user/tag/index.blade.php
+++ resources/views/pages/user/tag/index.blade.php
@@ -35,7 +35,7 @@
                                         </h3>
                                         <ul class="news-metainfo list-style">
                                             <li><i class="fi fi-rr-calendar-minus"></i><a
-                                                    href="javascript:void(0)">{{ $item->news->created_at->format('M d, Y') }}</a>
+                                                    href="javascript:void(0)">{{\Carbon\Carbon::parse($item->news->upload_date)->translatedFormat('d F Y') }}</a>
                                             </li>
                                             <li><i class="fi fi-rr-eye"></i>{{ $item->news->views->count() }}</li>
                                         </ul>
