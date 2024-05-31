7bc3d430a926c8b0a546fe59aa067342e127c9c3
paksiswanto <unitedkuryu@gmail.com>
2024-05-31 15:27:37 +0700

    Update singlepost.blade.php

--- resources/views/pages/user/news/singlepost.blade.php
+++ resources/views/pages/user/news/singlepost.blade.php
@@ -740,12 +740,13 @@
                                                                             <a class=""
                                                                                 href="{{ route('author.detail', ['id' => $comment->user->slug]) }}">
                                                                         @endif
-                                                                        @if ($comment->user->roles->pluck('name')[0] == "author" || "admin")
-                                                                            <span class="text-name-comment">
-                                                                                {{ $comment->user->name }} 
+                                                      
+                                                                        <span class="text-name-comment">
+                                                                            {{ $comment->user->name }} 
+                                                                            @if ($comment->user->roles->pluck('name')[0] === "author" ||$comment->user->roles->pluck('name')[0] === "admin")
                                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="m12.25 10.825l.925.925L18.6 6.325l-.925-.925zM5 19h.925l5.825-5.825l-.925-.925L5 18.075zm8.875-5.125l-3.75-3.75L14.3 5.95l-.725-.725L8.1 10.7L6.7 9.3l5.45-5.475q.6-.6 1.413-.6t1.412.6l.725.725l1.25-1.25q.3-.3.713-.3t.712.3L20.7 5.625q.3.3.3.712t-.3.713zM6.75 21H3v-3.75l7.125-7.125l3.75 3.75z"/></svg>
+                                                                                @endif
                                                                             </span>
-                                                                        @endif
 
                                                                         @if ($comment->user_id === $comment->news->user_id)
                                                                             <span style="font-size: 0.8em;font-weight:400;color:red"> -
@@ -936,12 +937,13 @@
                                                                                 <a
                                                                                     href="{{ route('author.detail', ['id' => $reply->user->slug]) }}">
                                                                             @endif
-                                                                            @if ($reply->user->roles->pluck('name')[0] == "author" || "admin")
+                                                                         
                                                                             <span class="text-name-comment">
                                                                                 {{ $reply->user->name }} 
+                                                                                @if ($reply->user->roles->pluck('name')[0] === "author" ||$reply->user->roles->pluck('name')[0] === "admin")
                                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="m12.25 10.825l.925.925L18.6 6.325l-.925-.925zM5 19h.925l5.825-5.825l-.925-.925L5 18.075zm8.875-5.125l-3.75-3.75L14.3 5.95l-.725-.725L8.1 10.7L6.7 9.3l5.45-5.475q.6-.6 1.413-.6t1.412.6l.725.725l1.25-1.25q.3-.3.713-.3t.712.3L20.7 5.625q.3.3.3.712t-.3.713zM6.75 21H3v-3.75l7.125-7.125l3.75 3.75z"/></svg>
+                                                                                @endif
                                                                             </span>
-                                                                        @endif
                                                                             @if ($reply->user_id === $reply->news->user_id)
                                                                                 <span style="font-size: 0.8em;font-weight:400;color:red"> -
                                                                                     pembuat</span>
