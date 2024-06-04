5d2c7d914d5c83d448acb94928900b9e145b7393
ArdianS18 <ardiansupriadi464@gmail.com>
2024-06-04 09:47:55 +0700

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -215,7 +215,7 @@
                                         style="object-fit: cover;" alt="Image" height="250" width="100%" />
                                 </a>
                                 <a href="{{ route('categories.show.user', ['category' => $barus->news->newsCategories[0]->category->slug]) }}"
-                                    class="news-cat">{{ $barus->news->newsCategories[0]->category->name }}</a>
+                                    class="news-cat">{{ $barus->category->name }}</a>
                             </div>
                             <div class="news-card-info">
                                 <h3><a data-toggle="tooltip" data-placement="top" title="{{ $barus->news->name }}"
