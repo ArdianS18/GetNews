745d03302b356d4c5593882b540ace446ffc4d59
ArdianS18 <ardiansupriadi464@gmail.com>
2024-06-04 13:58:52 +0700

    Update singlepost.blade.php

--- resources/views/pages/user/news/singlepost.blade.php
+++ resources/views/pages/user/news/singlepost.blade.php
@@ -813,7 +813,7 @@
 
                                         @foreach ($groupedReplies[$comment->id] ?? [] as $index => $reply)
                                         <div>
-                                            <div class="row reply-item w-100 ms-5 mt-4" style="display: {{ $index < 1 ? 'flex' : 'none' }};">
+                                            <div class="row reply-item w-100 ms-5 mt-4" style="display: {{ $index < 0 ? 'flex' : 'none' }};">
                                                 <div class="col-lg-1">
                                                     <div class="comment-author-img">
                                                         <img src="{{ asset($reply->user->photo ? 'storage/' . $reply->user->photo : 'default.png') }}" alt="Image" class="img-fluid" width="60" style="object-fit:cover; height: 60px;" />
